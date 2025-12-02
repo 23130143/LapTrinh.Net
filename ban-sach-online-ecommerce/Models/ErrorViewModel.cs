fd1dbf99bc4d478426975686e0215edc6533965d
Iloda251 <23130233@st.hcmuaf.edu.vn>
2025-12-02 23:37:37 +0700

    update folder project

---
+++ ban-sach-online-ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ban_sach_online_ecommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
